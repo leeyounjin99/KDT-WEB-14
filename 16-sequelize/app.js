b603d7ef01b0a3ef7ade7a7ddd8b5158e672d17d
LYJ <leeyounjin@naver.com>
2024-10-21 13:45:51 +0900

    sequelize study

--- 16-sequelize/app.js
+++ 16-sequelize/app.js
@@ -23,6 +23,8 @@ app.get("*", (req, res) => {
 });
 
 db.sequelize.sync({ force: false }).then(() => {
+  // force: false => 테이블이 없으면 생성
+  // force: true => 테이블 무조건 생성 (만약 테이블이 있다면 다 삭제하고 다시 생성 -> prod에서 사용 x)
   app.listen(PORT, () => {
     console.log(`http://localhost:${PORT}`);
   });
