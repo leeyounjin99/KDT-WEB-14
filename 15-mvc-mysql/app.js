99c3d103a10571686d71d4cb06a505894cbdea8c
LYJ <leeyounjin@naver.com>
2024-10-21 09:14:34 +0900

    mvc mysql study

--- 15-mvc-mysql/app.js
+++ 15-mvc-mysql/app.js
@@ -4,14 +4,15 @@ const PORT = 8000;
 
 app.set("view engine", "ejs");
 app.set("views", "./views");
-app.use("/static", express.static(__dirname + "/static"));
+app.use("/static", express.static(__dirname + "/static")); // 정적 파일 접근 위해
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // [라우터 분리]
 const indexRouter = require("./routes");
-app.use("/", indexRouter);
+app.use("/", indexRouter); // localhost:PORT/
 
+// [404 error] 맨 마지막 라우트로 선언
 app.get("*", (req, res) => {
   res.render("404");
 });
